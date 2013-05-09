4c023e57496d2442717b08a092a3998dd29a9a23
Ali Kheyrollahi <aliostad@gmail.com>
2013-05-09 18:13:08 +0100

    Adding sb.exe and remove unnecessary ilmerge

--- build.ps1
+++ build.ps1
@@ -5,4 +5,3 @@ param(
 )
 
 & "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $buildFile $buildParams $buildTarget
-& "tools\ilmerge.exe" /target:exe /out:bin\sb.exe /targetplatform:'v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319' artifacts\sb.exe artifacts\CommandLine.dll artifacts\Newtonsoft.Json.dll artifacts\System.Net.Http.Formatting.dll
