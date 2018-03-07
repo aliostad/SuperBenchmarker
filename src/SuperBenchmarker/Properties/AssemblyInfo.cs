874e298fefa719f09d9c8ee3623c1e9ed36f6d48
Ali Kheyrollahi <aliostad@gmail.com>
2018-03-07 14:23:13 +0000

    version 4.4

--- src/SuperBenchmarker/Properties/AssemblyInfo.cs
+++ src/SuperBenchmarker/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.3.0.0")]
-[assembly: AssemblyFileVersion("4.3.0.0")]
-[assembly: AssemblyInformationalVersion("4.3.0")]
+[assembly: AssemblyVersion("4.4.0.0")]
+[assembly: AssemblyFileVersion("4.4.0.0")]
+[assembly: AssemblyInformationalVersion("4.4.0")]
 
 [assembly: InternalsVisibleTo("SuperBenchmarker.Tests")]
